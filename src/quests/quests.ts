5656c31c710c0f17d93af226f7cebc924ac22b94
Moritz344 <moritzmaier34@gmail.com>
2026-06-16 20:28:58 +0200

    replace pagination component/add error handling for api requests

--- src/quests/quests.ts
+++ src/quests/quests.ts
@@ -1,40 +1,74 @@
-import { Component } from '@angular/core';
+import { Component,signal,inject } from '@angular/core';
 import { Topbar } from '../topbar/topbar';
 import { Requests } from '../service/requests';
 import { Item } from '../item/item';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Pagination } from '../pagination/pagination';
 
 
 @Component({
   selector: 'app-quests',
-  imports: [Topbar, Item, FormsModule, CommonModule],
+  imports: [Topbar, Item, FormsModule, CommonModule,Pagination],
   templateUrl: './quests.html',
   styleUrl: './quests.css',
 })
 export class Quests {
+  public data: any;
+  public searchParameter: { text: string, } = { text: "" };
+  public isLoading: boolean = false;
+  public pageData = signal<any>([]);
 
-  data: any;
-  searchParameter: { text: string, } = { text: "" };
-  isLoading: boolean = false;
+  public currentPage: number = 1;
+  public currentItemPage: number = 0;
+
+  public request = inject(Requests);
+
+  constructor() {
+    this.initData();
+  }
 
   initData() {
-    this.request.getQuests("").subscribe((response: any) => {
-      this.data = response.data;
+    this.request.getQuests(this.currentItemPage,"").subscribe({
+      next: (response: any) => {
+        this.data = response.data;
+        this.pageData.set(response.pagination);
+      },
+      error: (error: any) => {
+        console.error('Error fetching quests:', error);
+      }
     });
   }
 
   search() {
     this.isLoading = true;
     if (this.searchParameter.text.length < 2 && this.searchParameter.text != "") { return; };
-    this.request.getQuests(this.searchParameter).subscribe((response: any) => {
-      this.data = response.data;
-      this.isLoading = false;
+    this.request.getQuests(this.currentItemPage,this.searchParameter).subscribe({
+      next: (response: any) => {
+        this.data = response.data;
+        this.isLoading = false;
+      },
+      error: (error: any) => {
+        console.error('Error searching quests:', error);
+        this.isLoading = false;
+      }
     });
   }
 
-  constructor(private request: Requests) {
-    this.initData();
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.currentItemPage = page;
+
+    this.request.getQuests(this.currentItemPage, this.searchParameter).subscribe({
+      next: (response: any) => {
+        this.data = response.data;
+      },
+      error: (error: any) => {
+        console.error('Error fetching items page:', error);
+      }
+    });
+
   }
+
 
 }
